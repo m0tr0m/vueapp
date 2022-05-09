40f22915cd10b0d2721c571460093055404c0f3f
Romeo Mottola <r.mottola@ceyoniq.com>
2022-05-09 14:30:59 +0200

    Login and logout buttons + textarea

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { Ref, ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+
+const loginData: Ref<string|null> = ref('Nicht angemeldet')
+
 </script>
 
 <template>
 
-  <div class="flex w-full h-full justify-center items-center">
+  <div class="flex flex-col gap-4 w-full h-full justify-center items-center">
+    
     <h1 class="dark-world">{{msg}}</h1>
+
+    <div class="flex flex-col gap-4 border rounded-lg h-5/6 w-11/12 md:w-1/3 md:h-1/3 p-4">
+      <header class="flex gap-4 justify-end">
+        <button class="border px-4 py-2 rounded hover:bg-neutral-300 hover:text-black" type="button">
+          Login
+        </button>
+        <button class="border px-4 py-2 rounded hover:bg-neutral-300 hover:text-black" type="button">
+          Logout
+        </button>
+      </header>
+      <main class="flex justify-center items-center h-full overflow-auto">
+        <textarea class="dark-world w-full h-full" v-if="loginData" 
+          :value="loginData"></textarea>
+      </main>
+    </div>
+    
   </div>
 
 </template>
